28600ec0953efbd42496d760fe46d03b0bbabe46
Vimal Mishra <vimal.mishra_cs18@gla.ac.in>
2020-03-02 16:44:53 +0530

    main class added

---
+++ DSA/src/BST/main/MyMain.java
@@ -0,0 +1,16 @@
+package BST.main;
+
+import BST.tree.MyBST;
+
+public class MyMain {
+    public static void main(String[] args) {
+        MyBST bst = new MyBST();
+        bst.insert(50);
+        bst.insert(25);
+        bst.insert(75);
+        bst.insert(10);
+        bst.insert(35);
+        bst.insert(30);
+
+    }
+}
