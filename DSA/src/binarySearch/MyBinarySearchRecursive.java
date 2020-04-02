02a5afa0563b05db9083bc42e4ef4722e510ecff
Vimal Mishra <vimal.mishra_cs18@gla.ac.in>
2020-04-02 10:13:31 +0530

    some updation required

---
+++ DSA/src/binarySearch/MyBinarySearchRecursive.java
@@ -0,0 +1,17 @@
+package binarySearch;
+
+public class MyBinarySearchRecursive {
+    /*
+Algorithm : binarySearch(searchElement, low, high)
+Start:
+Step1: 		if(low > high) then step 2, else step 3.
+Step 2: 		return false;
+Step 3: 		mid = (low + high) / 2;
+Step 4:		 if(searchElement == arr[mid]) then step 5, else step 6
+Step 5:		 return true;
+Step 6:		 if(searchElement < arr[mid]) then step 7, else step 8
+Step 7:		 binarySearch(searchElement , low , mid - 1)
+Step 8:		 binarySearch(searchElement, mid + 1, high)
+end
+ */
+}
